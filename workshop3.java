0baea2e4666f25ea600b1f6e56c1833858f50c64
Abhisarika Khanal <abhisarikakhanal@gmail.com>
2025-11-19 09:50:30 +0545

    Initial sharing of project

---
+++ workshop3.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class workshop3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class workshop3
+{
+    int age;
+    static int qty;
+    
+    public static void main(String[]args)
+    {
+        //<className> <variable>=new <className>();
+        int age=18;
+        workshop3 v1= new workshop3();// calling static variable in static method
+        System.out.println(v1.age);
+        
+        System.out.println(age);
+        System.out.println(workshop3.qty);
+        
+        
+    }    
+}
